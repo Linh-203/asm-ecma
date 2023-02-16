4d84ad0866eed6b6b76754f87e8585f488b49ec3
Linh-203 <hlinh2003.5.9@gmail.com>
2023-02-16 22:10:11 +0700

    fix bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../components/footer";
+import Header from "../components/header";
 import Menu from "../components/menu";
 import About from "../components/about";
 import Projects from "../components/projects";
